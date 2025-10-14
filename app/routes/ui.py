cff553cc2431c44073fc3a24eb69b425df0a66bd
Tobias Vedå <tobiasved@uia.no>
2025-10-14 18:31:54 +0200

    created routes to open project

--- app/routes/ui.py
+++ app/routes/ui.py
@@ -1,5 +1,8 @@
 # Renders HTML templates (e.g., / → index.html, /editor → editor.html).
-from flask import Blueprint, current_app, jsonify
+
+from flask import Blueprint, current_app, jsonify, render_template, session
+
+from app.services.storage import new_project
 
 bp = Blueprint("ui", __name__)
 
@@ -18,3 +21,40 @@ def routes():
         "tools":  [f"{base}/tools/brush", f"{base}/tools/eraser"],
         "health": f"{base}/health"
     })
+
+#added for testing, not sure if 100% correct implementation
+@bp.get("/")
+def index():
+    return render_template("index.html")
+
+@bp.get("/editor")
+def editor():
+    return render_template("editor.html")
+
+@bp.get("/new_project")
+def open_new_project():
+    # create empty canvas in new storage folder
+    # save id in session
+    pid = new_project("abc", "new project")
+    session["pid"] = pid
+
+    return render_template("editor.html")
+
+@bp.get("/load_project")
+def open_loaded_project():
+    # load canvas from pickle in new storage folder
+    # save id in session
+    return render_template("editor.html")
+
+@bp.get("/get_layers")
+def get_layers():
+    # load canvas from pickle in new storage folder
+    # save id in session
+    return jsonify({
+        "selected_layer" : 0,
+        "layers" : [
+            {"name": "Background", "visible" : 1, "filename" : "bg.png"},
+            {"name": "Layer 1", "visible" : 1, "filename" : "l1.png"},
+            {"name": "Layer 2", "visible" : 1, "filename" : "l2.png"}
+        ]
+        }), 200
