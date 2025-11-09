15f96978ebc2fe41c3543a2841f080a01a9e13b9
Tobias Vedå <tobiasved@uia.no>
2025-11-09 19:12:03 +0100

    input validation for canvas size

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -95,8 +95,14 @@
 async function redirectNew(){
     let w = prompt("Canvas width in pixels");
     let h = prompt("Canvas height in pixels")
-    // let h=500
-    // let w=500
+    let pw = parseInt(w)
+    let ph = parseInt(h)
+    if (p != pw || pw < 1){
+        w = 500;
+    }
+    if (h != ph || ph < 1){
+        h = 500
+    }
     let res = await fetch("/api/v1/open_new_project", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
