b0c6c468af0ed5aba1b3cff402379e1939e7ba54
Bendik Tjøsvoll <bendikt@uia.no>
2025-11-10 14:13:04 +0100

    small fix in filters.js

--- app/static/js/filters.js
+++ app/static/js/filters.js
@@ -214,6 +214,7 @@ async function customKernel(event) {
         array.push(row);
     }
 
+    //
     // const data = {constant: 2, array: array };
     // await fetch("/api/v1/filters/kernel_filter", {
     //     method: "POST",
